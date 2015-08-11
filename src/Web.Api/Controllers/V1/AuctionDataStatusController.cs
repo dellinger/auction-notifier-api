2f762d5f38b40d127d5c6808ab18fc750849b698
David Ellinger <david.ellinger@me.com>
2015-08-10 18:42:25 -0700

    Added ApiVersionConstraints and a controller

---
+++ src/Web.Api/Controllers/V1/AuctionDataStatusController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Web.Api.Models;
+using Web.Common.Routing;
+
+namespace Web.Api.Controllers.V1
+{
+    [ApiVersion1RoutePrefix("AuctionDataStatus")]
+    public class AuctionDataStatusController : ApiController
+    {
+        public AuctionDataStatus getAuctionDataStatus()
+        {
+            // Return mock data for now
+            AuctionDataStatus auctionDataStatus = new AuctionDataStatus();
+            AuctionDataStatusResult auctionDataStatusResult = new AuctionDataStatusResult();
+
+            auctionDataStatusResult.Url = "tempUrl";
+            auctionDataStatusResult.LastModified = new DateTime().Date.ToBinary();
+            auctionDataStatus.AuctionDataStatusResult = new List<AuctionDataStatusResult>();
+            auctionDataStatus.AuctionDataStatusResult.Add(auctionDataStatusResult);
+            return auctionDataStatus;
+        }
+    }
+}
---
+++ src/Web.Common/Routing/ApiVersion1RoutePrefixAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace Web.Common.Routing
+{
+    public class ApiVersion1RoutePrefixAttribute : RoutePrefixAttribute
+    {
+        private const string RouteBase = "api/{apiVersion:apiVersionContraint(v1)}";
+        private const string PrefixRouteBase = RouteBase + "/";
+
+        public ApiVersion1RoutePrefixAttribute(string routePrefix)
+            : base(string.IsNullOrWhiteSpace(routePrefix) ? RouteBase : PrefixRouteBase + routePrefix)
+        { }
+    }
+}
